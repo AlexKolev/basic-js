ae6dd94523e52f174388817561422c9585a42eae
Defry <defryalex@gmail.com>
2022-04-25 17:35:40 +0500

    feat: make dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	//throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
+	//console.log(typeof members);
+	if (!Array.isArray(members)) {
+		return false;
+	}
+	let filretArr = members.filter(elem => typeof elem === "string")
+	return filretArr.map(elem => elem.trim()[0].toUpperCase()).sort().join('');
 }
 
 module.exports = {
-  createDreamTeam
+	createDreamTeam
 };
+
+console.log(createDreamTeam(['Matt', ' ann', 'Dmitry', 'Max']));// => 'ADMM'
+console.log(createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]));// => 'LOO'
+
+console.log(createDreamTeam(3));
