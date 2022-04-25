2ee204f30b050a2f2df7da91da94aaa31ab4bca1
Defry <defryalex@gmail.com>
2022-04-25 15:22:17 +0500

    feat: make cout cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,11 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+	//throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
+	let retCount = 0;
+	for (const matrixCol of matrix) {
+		for (const ceil of matrixCol) {
+			if (ceil === '^^') {
+				retCount++;
+			}
+		}
+	}
+	return retCount;
 }
 
 module.exports = {
-  countCats
+	countCats
 };
+
+console.log(countCats([
+	[0, 1, '^^'],
+	[0, '^^', 2],
+	['^^', 1, 2]
+]));
