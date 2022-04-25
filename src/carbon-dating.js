7900ac95b1aa28d5f839e23d75ad07a8dba3b2e5
Defry <defryalex@gmail.com>
2022-04-25 16:45:53 +0500

    feat: make carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,34 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+	//throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
+	//sampleActivity = +sampleActivity;
+	if (typeof sampleActivity != "string") {
+		return false;
+	}
+	sampleActivity = Number(sampleActivity);
+	if ((!isNaN(sampleActivity)) && sampleActivity > 0) {
+		let k = 0.693 / HALF_LIFE_PERIOD;
+		let t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+		if (t < 0) {
+			return false;
+		}
+		t = Math.ceil(t);
+		return t;
+	}
+	return false;
 }
 
 module.exports = {
-  dateSample
+	dateSample
 };
+
+
+console.log(dateSample('1'));// 22387 (or 22392 depending on formula used)
+console.log(dateSample('WOOT!'));// => false
+console.log(dateSample(-52892));
+console.log(dateSample('9000'));
+// console.log(dateSample('3'));
+
