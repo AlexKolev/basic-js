6f6b87ccbc80d8b87d62425c62743525ed91a06c
Defry <defryalex@gmail.com>
2022-04-27 15:59:44 +0500

    feat: make  extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+	//throw new NotImplementedError('Not implemented');
+	// remove line with error and write your code here
+	let retSrt = '';
+	retSrt += str;
+
+	let makeAddition = '';
+
+
+	if (options.hasOwnProperty('addition')) {
+		makeAddition += options.addition;
+		if (options.additionRepeatTimes && options.additionRepeatTimes > 0) {
+			for (let i = 2; i <= options.additionRepeatTimes; i++) {
+				if (options.additionSeparator) {
+					makeAddition = makeAddition + options.additionSeparator + options.addition;
+				} else {
+					makeAddition = makeAddition + '|' + options.addition;
+				}
+			}
+		}
+	}
+	retSrt += makeAddition;
+	if (options.repeatTimes && options.repeatTimes > 0) {
+		for (let i = 2; i <= options.repeatTimes; i++) {
+			if (options.separator) {
+				retSrt = retSrt + options.separator + (str + makeAddition);
+			} else {
+				retSrt = retSrt + '+' + (str + makeAddition);
+			}
+		}
+	}
+	return retSrt;
 }
 
 module.exports = {
-  repeater
+	repeater
 };
+
+console.log(repeater('la', { repeatTimes: 3 }));
+
+
+console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }));
+//, 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
+//   truefalse!!!false??? truefalse!!!false??? truefalse!!!false
+console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }));
+//, 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');
+//	 nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null
